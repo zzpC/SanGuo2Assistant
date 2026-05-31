ef5b65f85be3e50056b282f74deebfbe64c76485
zzpC <a1348778498@gmail.com>
2026-06-01 02:28:33 +0800

    Update KSP version and initialize project TODO list
    - Update the KSP (Kotlin Symbol Processing) plugin version to 2.3.2 in `build.gradle.kts`.
    - Add `todo.txt` to track planned tasks, including potential bottom navigation reordering and enhancements to the soldier selection UI.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:compose-compiler-gradle-plugin:2.2.10")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
-        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.2.10-2.0.2")
+        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.3.2")
     }
 }
 
